d24fcf016c6bd92d1d29acc79427b97af1d52e59
code_unlimited <137721178+Hossain86@users.noreply.github.com>
2024-01-31 23:52:27 +0600

    Update rock-peeper-scisssor.js

--- rock-peeper-scisssor.js
+++ rock-peeper-scisssor.js
@@ -2,30 +2,30 @@ let random_num;
     function myrock(){
       random_num=Math.random();
   if(random_num<=(1/3)){
-    alert('Computer picks Rock...its tie');
+    alert('Computer picks Rock...its tie 😶');
   }else if(random_num>(1/3) && random_num<= (2/3)){
-    alert('Computer picks Paper...You lost');
+    alert('Computer picks Paper...You lost 🥲');
   }else{
-    alert('Computer picks Scissor...You win');
+    alert('Computer picks Scissor...You win 😎');
   }
     }
     function mypaper(){
       random_num=Math.random();
   if(random_num<=(1/3)){
-    alert('Computer picks Rock...You win');
+    alert('Computer picks Rock...You win 😎');
   }else if(random_num>(1/3) && random_num<= (2/3)){
-    alert('Computer picks Paper...Its tie');
+    alert('Computer picks Paper...Its tie 😶');
   }else{
-    alert('Computer picks Scissor...You lost');
+    alert('Computer picks Scissor...You lost 🥲');
   }
     }
     function myscissor(){
    random_num=Math.random();
   if(random_num<=(1/3)){
-    alert('Computer picks Rock...You lost');
+    alert('Computer picks Rock...You lost 🥲');
   }else if(random_num>(1/3) && random_num<= (2/3)){
-    alert('Computer picks Paper...you win');
+    alert('Computer picks Paper...you win 😎');
   }else{
-    alert('Computer picks Scissor...its tie');
+    alert('Computer picks Scissor...its tie 😶');
   }
     }
