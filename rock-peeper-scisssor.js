c414bf313037b003e27b6a978dff73f6718ba962
code_unlimited <137721178+Hossain86@users.noreply.github.com>
2024-02-02 21:55:27 +0600

    Update rock-peeper-scisssor.js

--- rock-peeper-scisssor.js
+++ rock-peeper-scisssor.js
@@ -1,31 +1,63 @@
 let random_num;
+const result={
+  win: 0,
+  loss: 0,
+  tie:0
+}
+//console.log(result.win)
     function myrock(){
       random_num=Math.random();
   if(random_num<=(1/3)){
-    alert('Computer picks Rock...its tie 😶');
+    result.tie+=1;
+    alert(`Computer picks Rock...its tie 😶
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
+    
   }else if(random_num>(1/3) && random_num<= (2/3)){
-    alert('Computer picks Paper...You lost 🥲');
+    result.loss+=1;
+    alert(`Computer picks Paper...You lost 🥲
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
+    
   }else{
-    alert('Computer picks Scissor...You win 😎');
+    result.win+=1;
+    alert(`Computer picks Scissor...You win 😎
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
+    
   }
     }
     function mypaper(){
       random_num=Math.random();
   if(random_num<=(1/3)){
-    alert('Computer picks Rock...You win 😎');
+    result.win+=1;
+    alert(`Computer picks Rock...You win 😎
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
+    
   }else if(random_num>(1/3) && random_num<= (2/3)){
-    alert('Computer picks Paper...Its tie 😶');
+    result.tie+=1;
+    alert(`Computer picks Rock...its tie 😶
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
+    
   }else{
-    alert('Computer picks Scissor...You lost 🥲');
+    result.loss+=1;
+    alert(`Computer picks Scissor...You lost 🥲
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
+    
   }
     }
     function myscissor(){
    random_num=Math.random();
   if(random_num<=(1/3)){
-    alert('Computer picks Rock...You lost 🥲');
+    result.loss+=1;
+    alert(`Computer picks Rock...You lost 🥲
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
+    
   }else if(random_num>(1/3) && random_num<= (2/3)){
-    alert('Computer picks Paper...you win 😎');
+     result.win+=1;
+    alert(`Computer picks Paper...you win 😎
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
+   
   }else{
-    alert('Computer picks Scissor...its tie 😶');
+    result.tie+=1;
+    alert(`Computer picks Rock...its tie 😶
+Wins: ${result.win}, Lost: ${result.loss}, Tie: ${result.tie}`);
   }
     }
